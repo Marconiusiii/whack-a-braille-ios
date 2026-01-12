5392f17390696b3d9d7bf39d6c04601fe11e5dd2
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-01-12 15:00:18 -0800

    Initial Commit

---
+++ Whack A Braille/Whack_A_BrailleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whack_A_BrailleApp.swift
+//  Whack A Braille
+//
+//  Created by Marco Salsiccia on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Whack_A_BrailleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
